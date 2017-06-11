049ade67fd82578f5667a93c60505fa90783c684
Andrei Kurnits <fake@gmail.com>
2017-06-11 23:06:04 +0300

    Right works but you can't adjust pause between requests

--- minshweather.sh
+++ minshweather.sh
@@ -5,10 +5,15 @@
 site="https://pogoda.tut.by/city/minsk/"
 page="index.html"
 
-wget $site &> /dev/null
-
 echo -n "Temperature in Minsk now is "
-
-cat $page | grep temp-i | head -1 | cut -c22-24 
-
-rm $page
+while [ 1 ]
+do
+    wget $site &> /dev/null
+    temperature=`cat $page | grep temp-i | head -1 | cut -c22-24`
+    rm $page
+    echo -n $temperature
+    sleep 2
+    echo -n $'\b'
+    echo -n $'\b'
+    echo -n $'\b'
+done
